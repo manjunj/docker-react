0053b6f814bcf4ec6c4d5d8872ffd94526f27611
manjunj <manjunj@student.unimelb.edu.au>
2022-03-05 15:42:52 +1100

    change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on feature branch
+          Learn
         </a>
       </header>
     </div>
